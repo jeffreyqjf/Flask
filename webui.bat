bcf89ebe05a78c89a83f60e33329ab72da436521
Jeffrey <3172268428@qq.com>
2023-06-19 14:26:08 +0800

    webui version

--- webui.bat
+++ webui.bat
@@ -3,11 +3,11 @@
 
 REM 检查虚拟环境是否已经创建
 IF EXIST venv (
-    echo 虚拟环境已创建
+    
     REM 激活虚拟环境
     
     call venv\Scripts\activate
-    python -m ensurepip --upgrade
+    
     pip install -r requirements.txt
 
 
@@ -17,13 +17,13 @@ IF EXIST venv (
     REM 退出虚拟环境
     deactivate
 ) ELSE (
-    echo 虚拟环境尚未创建
+  
     REM 创建虚拟环境
     python -m venv venv
 
     REM 激活虚拟环境
     call venv\Scripts\activate
-    python -m ensurepip --upgrade
+    
     REM 安装所需的Python包
     pip install -r requirements.txt
 
