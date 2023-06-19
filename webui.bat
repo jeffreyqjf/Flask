86f8502ee83c0a26f38cefc5b50a5c5e96f243cd
Jeffrey <3172268428@qq.com>
2023-06-19 12:49:59 +0800

    webui version

--- webui.bat
+++ webui.bat
@@ -1,3 +1,32 @@
-call venv\Scripts\activate
-python run.py
-pause
+@echo off
+
+
+REM 检查虚拟环境是否已经创建
+IF EXIST venv (
+    echo 虚拟环境已创建
+    REM 激活虚拟环境
+    
+    call venv\Scripts\activate
+    python -m ensurepip --upgrade
+    pip install -r requirements.txt
+
+
+    python run.py
+
+    pause
+    REM 退出虚拟环境
+    deactivate
+) ELSE (
+    echo 虚拟环境尚未创建
+    REM 创建虚拟环境
+    python -m venv venv
+
+    REM 激活虚拟环境
+    call venv\Scripts\activate
+    python -m ensurepip --upgrade
+    REM 安装所需的Python包
+    pip install -r requirements.txt
+
+    REM 退出虚拟环境
+    deactivate
+)
