e4722855d78683ddc95e9cdc9feb46cd89dec3d9
Jeffrey <3172268428@qq.com>
2023-06-20 13:46:55 +0800

    fix secret_key_2

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -10,6 +10,7 @@ app.config['MAIL_PASSWORD'] = 'byxubvgykznvddef'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['SECRET_KET'] = 'hard to guess'
+app.secret_key = 'hard to guess'
 mail = Mail(app)
 
 from my_app import urls
