aab5a129305558a242a9be3558a3f8fcd5593a15
Jeffrey <3172268428@qq.com>
2023-06-20 13:17:46 +0800

    webui improve version

--- webui.bat
+++ webui.bat
@@ -1,12 +1,15 @@
 @echo off
 
+if not defined PYTHON (set PYTHON=python)
+if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
 
 REM 检查虚拟环境是否已经创建
-IF EXIST venv (
+IF EXIST %VENV_DIR% (
     
     REM 激活虚拟环境
     
-    call venv\Scripts\activate
+    call %VENV_DIR%\Scripts\activate
+    echo %VENV_DIR%
     python.exe -m pip install --upgrade pip
     pip install -r requirements.txt
 
@@ -22,7 +25,7 @@ IF EXIST venv (
     python -m venv venv
 
     REM 激活虚拟环境
-    call venv\Scripts\activate
+    call %VENV_DIR%\Scripts\activate
     python.exe -m pip install --upgrade pip
     REM 安装所需的Python包
     pip install -r requirements.txt
